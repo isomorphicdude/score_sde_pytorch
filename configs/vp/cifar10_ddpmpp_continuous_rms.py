f53818650fe6db9657e66cf97ed55e41e4d28158
isomorphicdude <iamthesenate@qq.com>
2023-07-12 18:08:42 +0800

    add new configurations

--- configs/vp/cifar10_ddpmpp_continuous_rms.py
+++ configs/vp/cifar10_ddpmpp_continuous_rms.py
@@ -7,28 +7,33 @@ def get_config():
   config = get_default_configs()
   # training
   training = config.training
-  training.sde = 'vesde'
+  training.sde = 'vpsde'
   training.continuous = True
+  training.reduce_mean = True
 
   # sampling
   sampling = config.sampling
   sampling.method = 'pc'
   sampling.predictor = 'rms_reverse_diffusion'
-  sampling.corrector = 'rms_langevin'
+  sampling.corrector = 'none'
   sampling.extra_args = {'lr': 0.1,
                          'sde_lr':100,
                          'beta1': 0.999,
                          'beta2': 0.99,
                          'beta3': 0,
                          'beta4': 0}
-  # sampling.n_steps_each = 2
+  
   sampling.use_preconditioner = True
+
+  # data
+  data = config.data
+  data.centered = True
 
   # model
   model = config.model
   model.name = 'ncsnpp'
-  model.scale_by_sigma = True
-  model.ema_rate = 0.999
+  model.scale_by_sigma = False
+  model.ema_rate = 0.9999
   model.normalization = 'GroupNorm'
   model.nonlinearity = 'swish'
   model.nf = 128
@@ -37,17 +42,19 @@ def get_config():
   model.attn_resolutions = (16,)
   model.resamp_with_conv = True
   model.conditional = True
-  model.fir = True
+  model.fir = False
   model.fir_kernel = [1, 3, 3, 1]
   model.skip_rescale = True
   model.resblock_type = 'biggan'
   model.progressive = 'none'
-  model.progressive_input = 'residual'
+  # missing residual compared to cifar10_ncsnpp_continuous.py
+  # only weights for this model are in the checkpoint
+  model.progressive_input = 'none' 
   model.progressive_combine = 'sum'
   model.attention_type = 'ddpm'
   model.init_scale = 0.
+  model.embedding_type = 'positional'
   model.fourier_scale = 16
   model.conv_size = 3
-  model.modified_sde = True
 
   return config
